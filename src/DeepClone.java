bf5a5b4c378810742e3941e300809b1a7340163a
aman-goyal-007 <amangoyal007@gmail.com>
2019-01-11 15:59:05 +0530

    Deep Clone

--- src/DeepClone.java
+++ src/DeepClone.java
@@ -1,12 +1,13 @@
-import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
 
 // Need to create deep cloning using this
+/*
+Deep clocning is not a good idea. Instead class should provide copy constructor if required.
+ */
+
 public class DeepClone {
 
-	public static void main(String...args) throws CloneNotSupportedException, ClassNotFoundException, InstantiationException, IllegalAccessException, IllegalArgumentException, NoSuchFieldException, SecurityException{
+	public static void main(String...args) throws ClassNotFoundException, InstantiationException, IllegalAccessException{
 		Child c = new Child();
 		c.childName="child";
 		c.p.name="parent";
@@ -15,12 +16,10 @@ public class DeepClone {
 		
 	}
 	
-	public static Object getClone(Object o,Object clone) throws IllegalArgumentException, IllegalAccessException, ClassNotFoundException, NoSuchFieldException, SecurityException, InstantiationException{
+	private static void getClone(Object o,Object clone) throws IllegalAccessException, ClassNotFoundException, InstantiationException{
 		Class myClass = o.getClass();
-		Class clonedClass=clone.getClass();
-		Field[] clonedFields = clonedClass.getDeclaredFields();
+
 		Field[] c = myClass.getDeclaredFields();
-		int i=0;
 		for(Field f:c){
 			Class type=f.getType();
 			if(type==int.class || type==char.class || type==double.class || type==float.class){
@@ -30,12 +29,8 @@ public class DeepClone {
 				System.out.println(f.getName());
 				System.out.println(Class.forName(type.getName()).newInstance());
 				f.get(type.newInstance());
-//				getClone(f.get(f.getName()),clonedFields[i]);
-	//			System.out.println(clonedFields[i]);
 			}
-			i++;
 		}
-		return clone;
 	}
 	
 }
